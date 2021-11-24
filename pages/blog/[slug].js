eb38a608d223e089040ea3f846249a1cc2a43b82
Rodrigo Valente <valentedev.rodrigo@gmail.com>
2021-11-24 14:25:09 -0300

    add style to single blog post

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,13 +1,44 @@
 import fs from "fs";
+import Link from "next/link";
+import { marked } from "marked";
 import path from "path";
 import matter from "gray-matter";
+import Layout from "../../components/Layout";
+import CategoryLabel from "../../components/CategoryLabel";
 
 export default function PostPage({
-  frontmatter: { title, category, data, cover_image, author, author_image },
+  frontmatter: { title, category, date, cover_image, author, author_image },
   content,
   slug,
 }) {
-  return <div>{title}</div>;
+  return (
+    <Layout title={"Blog | " + `${title}`}>
+      <Link href="/blog">Go back</Link>
+      <div className="w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6">
+        <div className="flex justify-between items-center mt-4">
+          <h1 className="text-5xl mb-7">{title}</h1>
+          <CategoryLabel>{category}</CategoryLabel>
+        </div>
+        <img src={cover_image} alt="" className="w-full rounded" />
+
+        <div className="flex justify-between items-center bg-gray-100 p-2 my-8">
+          <div className="flex items-center">
+            <img
+              src={author_image}
+              alt=""
+              className="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"
+            />
+            <h4>{author}</h4>
+          </div>
+          <div className="mr-4">{date}</div>
+        </div>
+
+        <div className="blog-text mt-2">
+          <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
 export async function getStaticPaths() {
